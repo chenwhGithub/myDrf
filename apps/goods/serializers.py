08bf2d4885cba16c48e51faa0e40984d2f4a484a
wenhchen <wenhui.chen@nokia-sbell.com>
2021-05-13 16:48:28 +0800

    add validate for POST procedure

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from .models import Goods, Category
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -17,8 +18,50 @@ class GoodsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Goods
         # fields = '__all__'
-        fields = ['category', 'category_id', 'name', 'click_num', 'sold_num', 'fav_num',
+        fields = ['id', 'category', 'category_id', 'name', 'click_num', 'sold_num', 'fav_num',
                   'storage_num', 'price', 'descript', 'image', 'is_hot', 'add_time']
+        extra_kwargs = {
+            'price': {
+                'required': True, # 设置字段静态校验规则，通常在 model 中定义即可
+                'max_value': 9999,
+                'error_messages': {
+                    # 设置 Response(serialzer.errors) 返回的错误信息，如下：
+                    # {
+                    #     "price": [
+                    #         "price 字段是必填项"
+                    #     ]
+                    # }
+                    'required': 'price 字段是必填项',
+                    'max_value': '价格不能大于 9999',
+                }
+            }
+        }
+
+    # 局部钩子，单个字段逻辑校验，函数名格式：validate_字段名
+    def validate_name(self, value):
+        if 'hello' in value.lower():
+            # 设置 Response(serialzer.errors) 返回的错误信息，如下：
+            # {
+            #     "name": [
+            #         "名字不能包含 hello 字符"
+            #     ]
+            # }
+            raise ValidationError('名字不能包含 hello 字符')
+        return value
+
+    # 全局钩子，所有字段逻辑校验，可以对多个字段进行联合校验
+    def validate(self, attrs):
+        cat_id = attrs.get('category_id')
+        name = attrs.get('name')
+        if Goods.objects.filter(name=name, category_id=cat_id):
+            # 设置 Response(serialzer.errors) 返回的错误信息，如下：
+            # {
+            #     "name_and_category_id": [
+            #         "名字和分类都相同，该书已存在"
+            #     ]
+            # }
+            raise ValidationError({'name_and_category_id':'名字和分类都相同，该书已存在'})
+        return attrs
 
     def create(self, validated_data):
         cat_id = validated_data.pop('category_id')
